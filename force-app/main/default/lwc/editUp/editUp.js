d8e57d40115c57abf0c0b26f9f90bf3d9e132b14
ccfurr <ccfurr@go.olemiss.edu>
2026-03-22 15:13:35 +0000

    Update stuff
    Yeah

--- force-app/main/default/lwc/editUp/editUp.js
+++ force-app/main/default/lwc/editUp/editUp.js
@@ -4,16 +4,18 @@ import ModalManage from 'c/modalManage';
 export default class EditUp extends LightningElement {
     latestQueryName = '';
 
+    // getting latest query from localstorage to dispaly on page
     connectedCallback() {
         this.latestQueryName = localStorage.getItem('latestQueryName') || '';
     }
 
+    // waiting for case user open the card -> open modal
     async openModal() {
         const result = await ModalManage.open({
             size: 'medium',
             description: 'Manage queries'
         });
-
+        
         this.latestQueryName = localStorage.getItem('latestQueryName') || '';
 
         if (result && result.refresh) {
